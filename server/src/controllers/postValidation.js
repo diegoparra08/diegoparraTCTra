b9397491c806a02650c8b8c5501f1774ef6679cc
diegoalparra <diegoalparra08@gmail.com>
2023-10-28 15:45:07 -0500

    post request works in server

---
+++ server/src/controllers/postValidation.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+require('dotenv').config();
+
+const { API_KEY } = process.env;
+
+async function postValidation(req, res) {
+    try {
+
+        const response = await axios.post('https://api.validations.truora.com/v1/validations', {
+            type: 'document-validation',
+            country: 'CO',
+            document_type: 'national-id',
+            user_authorized: true,
+        },
+            {
+                headers: {
+                    'Truora-API-Key': API_KEY,
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+            });
+
+        const responseData = response.data;
+        console.log('Acá viene la respuesta....', responseData, 'hasta acá');
+        // console.log('Obtengo F URL', responseData.instructions.front_url);
+        // console.log('Obtengo Validation ID', responseData.validation_id);
+
+        res.status(200).json(responseData);
+    } catch (error) {
+        console.error('Error en la solicitud POST:', error);
+
+        res.status(500).json({ error: 'Error en la solicitud POST' });
+    }
+}
+
+module.exports = postValidation;
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,12 +1,12 @@
 const { Router } = require('express')
 
-// const postValidation = require('../controllers/postValidation');
+const postValidation = require('../controllers/postValidation');
 const getValidationStatus = require('../controllers/getValidationStatus');
 // const putDocument = require('../controllers/putDocument');
 
 const router = Router();
 
-// router.post('/', postValidation);
+router.post('/', postValidation);
 router.get('/', getValidationStatus);
 // router.put('/', putDocument);
 
